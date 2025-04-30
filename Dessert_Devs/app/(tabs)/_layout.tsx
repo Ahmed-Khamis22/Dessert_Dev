f3117bcb87d38723250a916385431ac65a156cc2
Shahd Hesham <shoudyhesham2003@gmail.com>
2025-05-01 01:44:28 +0300

    commit

--- Dessert_Devs/app/(tabs)/_layout.tsx
+++ Dessert_Devs/app/(tabs)/_layout.tsx
@@ -1,10 +1,79 @@
+// app/(tabs)/_layout.tsx
 import { Tabs } from 'expo-router';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import AuthGuard from '../../authGuard'; // تأكد من الباث الصحيح
+import Toast from 'react-native-toast-message';
 
 export default function TabLayout() {
   return (
-    <Tabs screenOptions={{ headerShown: false }}>
-      <Tabs.Screen name="index" options={{ href: null }} /> {/* Hide splash tab */}
-      <Tabs.Screen name="products" options={{ title: 'Products' }} />
-    </Tabs>
+      <>
+        <AuthGuard>
+          <Tabs
+            screenOptions={{
+              tabBarActiveTintColor: '#f48fb1', // وردي نشط
+              tabBarInactiveTintColor: '#ffffff', // أبيض هادي
+              tabBarStyle: {
+                backgroundColor: '#4e342e', // بني غامق متناسق مع الأزرار
+                borderTopWidth: 0,
+                height: 65,
+                paddingBottom: 10,
+                paddingTop: 6,
+              },
+              tabBarLabelStyle: {
+                fontSize: 13,
+                fontWeight: '600',
+              },
+            }}
+          >
+          <Tabs.Screen 
+            name="index"
+            options={{
+              headerShown: false,
+              title: 'Home',
+              tabBarIcon: ({ color, focused }) => (
+                <Ionicons name={focused ? 'home-sharp' : 'home-outline'} color={color} size={28} />
+              ),
+            }}
+          />
+        
+          <Tabs.Screen
+            name="about"
+            options={{
+              headerShown: false,
+              title: 'About',
+              tabBarIcon: ({ color, focused }) => (
+                <Ionicons name={focused ? 'information-circle' : 'information-circle-outline'} color={color} size={28} />
+              ),
+            }}  
+          />
+        
+          <Tabs.Screen
+            name="products"
+            options={{
+              headerShown: false,
+              title: 'Products',
+              tabBarIcon: ({ color, focused }) => (
+                <Ionicons name={focused ? 'cart-sharp' : 'cart-outline'} color={color} size={28} />
+              ),
+            }}
+          />
+
+          // For profile
+          <Tabs.Screen
+            name="profile"
+            options={{
+              headerShown: false, // عشان أشيل الجزء الأبيض اللي كان فوق في البروفايل 
+              title: "Profile",
+              tabBarIcon: ({ color, size }) => (
+                <Ionicons name="person" size={size} color={color} />
+              ),
+            }}
+          />
+        
+          </Tabs>
+        </AuthGuard>
+
+        <Toast/> 
+      </>      
   );
 }
